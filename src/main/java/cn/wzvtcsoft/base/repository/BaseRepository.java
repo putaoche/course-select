2238e47a2bbe99d7620ddcd917666c076b22eae9
user <1171874514@qq.com>
2019-05-08 11:00:47 +0800

    fix

--- src/main/java/cn/wzvtcsoft/base/repository/BaseRepository.java
+++ src/main/java/cn/wzvtcsoft/base/repository/BaseRepository.java
@@ -1,7 +1,9 @@
 package cn.wzvtcsoft.base.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+@NoRepositoryBean
 public interface BaseRepository<T> extends JpaRepository<T, Long>, PagingAndSortingRepository<T,Long> {
 }
